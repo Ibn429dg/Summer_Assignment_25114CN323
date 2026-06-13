49cd9e8c1db87386480f4cd5d9da172311064dd0
Aditya Yadav <adityayadav@Adityas-MacBook-Air.local>
2026-06-13 23:22:30 +0530

    Added DAY_12

---
+++ DAY_12/FunctionForPalindrome.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPalindrome(int n)
+{
+    int original = n, reverse = 0, rem;
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        reverse = reverse * 10 + rem;
+        n = n / 10;
+    }
+
+    return (original == reverse);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num))
+        printf("%d is a Palindrome Number.\n", num);
+    else
+        printf("%d is Not a Palindrome Number.\n", num);
+
+    return 0;
+}
